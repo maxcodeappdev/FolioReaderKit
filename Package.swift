faa9203a3dab00c66cc80949e9b926c8459da46f
Max Contreras <maxcodeappdev@gmail.com>
2025-03-26 07:53:35 -0700

    Fixed AEXML URL

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.1.0"),
         .package(url: "https://github.com/cxa/MenuItemKit.git", from: "3.0.0"),
         .package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", from: "0.6.0"),
-        .package(url: "https://github.com/tadija/AEXML.git", from: "4.2.0"),
+        .package(url: "https://github.com/tadija/AEXML.git", from: "4.7.0"),
         .package(url: "https://github.com/ArtSabintsev/FontBlaster.git", from: "4.0.0"),
         .package(url: "https://github.com/fantim/JSQWebViewController.git", from: "6.1.0"),
         .package(url: "https://github.com/realm/realm-cocoa.git", from: "3.1.0")
